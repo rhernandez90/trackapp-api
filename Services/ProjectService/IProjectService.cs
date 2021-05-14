911d8d2a8120bacb3df64abe7a3cff1aba03c8a0
rhernandez90 <rhernandezmunguia@gmail.com>
2021-05-13 22:28:32 -0600

    Add Project Service with create function

---
+++ Services/ProjectService/IProjectService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.Services.ProjectService.Dto;
+
+namespace WebApi.Services.ProjectService
+{
+    public interface IProjectService
+    {
+        Task<ProjectDto> Create(ProjectDto ProjectData);
+    }
+}
---
+++ Services/ProjectService/ProjectService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.Entities;
+using WebApi.Helpers;
+using WebApi.Services.ProjectService.Dto;
+
+namespace WebApi.Services.ProjectService
+{
+    public class ProjectService : IProjectService
+    {
+        private readonly DataContext _context;
+        public ProjectService(
+            DataContext context   
+        )
+        {
+            _context = context;
+        }
+
+
+        public async Task<ProjectDto> Create(ProjectDto ProjectData)
+        {
+            var project = new Project()
+            {
+                Description = ProjectData.Description,
+                Name = ProjectData.Name
+            };
+
+            await _context.Projects.AddAsync(project);
+            ProjectData.Id = project.Id;
+            return ProjectData;
+        }
+    }
+}
