adba47c3797fe9080703bd5ce8a9bceb3f8f271b
rhernandez90 <rhernandezmunguia@gmail.com>
2021-05-15 16:09:07 -0600

    add PersonController with Crud End Points

--- Controllers/PersonController.cs
+++ Controllers/PersonController.cs
@@ -25,18 +25,56 @@ namespace WebApi.Controllers
 
         [Authorize(Roles = "Role.Admin")]
         [HttpPost("")]
-        public async Task<ActionResult> Create([FromBody] PersonDto taskData)
+        public async Task<ActionResult> Create([FromBody] PersonDto personData)
         {
             try
             {
-                var person = await _personService.Create(taskData);
-                return Ok(task);
+                var person = await _personService.Create(personData);
+                return Ok(person);
             }
             catch (Exception ex)
             {
                 return BadRequest(new { message = $"{ex.Message}, {ex.InnerException}" });
             }
 
+        }
+
+
+        [Authorize(Roles = "Role.Admin")]
+        [HttpGet("")]
+        public async Task<ActionResult> GetAll()
+        {
+            var persons = await _personService.GetAll();
+            return Ok(persons);
+        }
+
+        [Authorize]
+        [HttpGet("{Id}")]
+        public async Task<ActionResult> GetById(int id)
+        {
+            var person = await _personService.GetById(id);
+            if (person == null)
+                return NotFound();
+
+            return Ok(person);
+        }
+
+        [Authorize]
+        [HttpPut("{Id}")]
+        public async Task<ActionResult> Update(int Id, PersonDto taskData)
+        {
+            try
+            {
+                var person = await _personService.Update(Id, taskData);
+                if (person == null)
+                    return NotFound();
+
+                return Ok(person);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = ex.Message });
+            }
         }
     }
 }
